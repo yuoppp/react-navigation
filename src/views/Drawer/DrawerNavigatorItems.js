fa3e894e26fac16a96409f3373b99413b67f69fb
Dave Pack <davepack@icloud.com>
2017-10-25 16:00:25 -0700

    Fix merge conflict.

--- src/views/Drawer/DrawerNavigatorItems.js
+++ src/views/Drawer/DrawerNavigatorItems.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import { View, Text, Platform, StyleSheet } from 'react-native';
 
+import SafeAreaView from '../SafeAreaView';
 import TouchableItem from '../TouchableItem';
 
 import type {
@@ -49,40 +50,49 @@ const DrawerNavigatorItems = ({
   itemStyle,
   labelStyle,
 }: Props) => (
-  <View style={[styles.container, itemsContainerStyle]}>
-    {items.map((route: NavigationRoute, index: number) => {
-      const focused = activeItemKey === route.key;
-      const color = focused ? activeTintColor : inactiveTintColor;
-      const backgroundColor = focused
-        ? activeBackgroundColor
-        : inactiveBackgroundColor;
-      const scene = { route, index, focused, tintColor: color };
-      const icon = renderIcon(scene);
-      const label = getLabel(scene);
-      return (
-        <TouchableItem
-          key={route.key}
-          onPress={() => {
-            onItemPress({ route, focused });
-          }}
-          delayPressIn={0}
-        >
-          <View style={[styles.item, { backgroundColor }, itemStyle]}>
-            {icon ? (
-              <View style={[styles.icon, focused ? null : styles.inactiveIcon]}>
-                {icon}
-              </View>
-            ) : null}
-            {typeof label === 'string' ? (
-              <Text style={[styles.label, { color }, labelStyle]}>{label}</Text>
-            ) : (
-              label
-            )}
-          </View>
-        </TouchableItem>
-      );
-    })}
-  </View>
+  <SafeAreaView insetOverride={{ horizontal: 0 }}>
+    <View style={[styles.container, itemsContainerStyle]}>
+      {items.map((route: NavigationRoute, index: number) => {
+        const focused = activeItemKey === route.key;
+        const color = focused ? activeTintColor : inactiveTintColor;
+        const backgroundColor = focused
+          ? activeBackgroundColor
+          : inactiveBackgroundColor;
+        const scene = { route, index, focused, tintColor: color };
+        const icon = renderIcon(scene);
+        const label = getLabel(scene);
+        return (
+          <TouchableItem
+            key={route.key}
+            onPress={() => {
+              onItemPress({ route, focused });
+            }}
+            delayPressIn={0}
+          >
+            <SafeAreaView
+              style={[styles.item, { backgroundColor }, itemStyle]}
+              insetOverride={{ vertical: 0 }}
+            >
+              {icon ? (
+                <View
+                  style={[styles.icon, focused ? null : styles.inactiveIcon]}
+                >
+                  {icon}
+                </View>
+              ) : null}
+              {typeof label === 'string' ? (
+                <Text style={[styles.label, { color }, labelStyle]}>
+                  {label}
+                </Text>
+              ) : (
+                label
+              )}
+            </SafeAreaView>
+          </TouchableItem>
+        );
+      })}
+    </View>
+  </SafeAreaView>
 );
 
 /* Material design specs - https://material.io/guidelines/patterns/navigation-drawer.html#navigation-drawer-specs */
@@ -95,7 +105,7 @@ DrawerNavigatorItems.defaultProps = {
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: Platform.OS === 'ios' ? 20 : 0,
+    // marginTop: Platform.OS === 'ios' ? 20 : 0,
     paddingVertical: 4,
   },
   item: {

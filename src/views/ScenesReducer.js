8666bb0ccced01bf7c744d3dad334e9e9d91a6c8
Yuri Tinyukov <y.tinyukov@qsoft.ru>
2017-10-30 14:19:47 +0300

    Workaround for flashing screens

--- src/views/ScenesReducer.js
+++ src/views/ScenesReducer.js
@@ -151,7 +151,22 @@ export default function ScenesReducer(
     }
   };
 
-  staleScenes.forEach(mergeScene);
+
+// work around for flashing scenes
+
+     let k = null;
+     let v = null;
+     staleScenes.forEach(scene => {
+       let {key} = scene;
+       k = key;
+       v = scene;
+     });
+
+     newStaleScenes = k && v ? new Map([[k, v]]) : new Map();
+     newStaleScenes.forEach(mergeScene);
+     // staleScenes.forEach(mergeScene);
+     // work around end
+
   freshScenes.forEach(mergeScene);
 
   nextScenes.sort(compareScenes);
